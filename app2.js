2a7a6400347695a6f526dc75289f6a73fc8e1bef
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-10-27 23:14:04 -0600

    push

--- app2.js
+++ app2.js
@@ -1,11 +1,9 @@
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
+colors.length;
 
-colors.length; //7
+colors[0];
 
-//Access elements using index:
-colors[0]; //'red'
+colors[6];
 
-colors[6]; //'violet'
-
-colors[7]; //undefined
+colors[7];
