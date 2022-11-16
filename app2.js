dfdf03058408fa4cf35828e1ee147b69320706fe
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-11-15 18:40:02 -0700

    push

--- app2.js
+++ app2.js
@@ -1,9 +1,12 @@
-let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-
-colors.length;
-
-colors[0];
-
-colors[6];
-
-colors[7];
+function palindrome(str) {
+  str = str
+    .toLowerCase()
+    .replace(/(\W)|(\_)/g, "")
+    .split("");
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
