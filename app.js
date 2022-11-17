a5d426bf25290375bdaa84411a1ab0f5d4131d8c
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-11-17 16:02:27 -0700

    push

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 let myVar = 87;
 
-/
+// Only change code below this line
+myVar = myVar + 1;
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
